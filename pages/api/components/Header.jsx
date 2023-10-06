8c58c98ba54470614d0542c4967033c498aee3be
Mehdi <94266592+mehdi-repo@users.noreply.github.com>
2023-10-05 22:37:57 -0400

    intial Header

--- pages/api/components/Header.jsx
+++ pages/api/components/Header.jsx
@@ -1,5 +1,24 @@
+import classes from "../../../styles/Header.module.css";
+import thumbnail from "../../../public/asset/portfolio-pic.png";
+import icSearch from "../../../public/images/ic-search.svg";
 const Header = () => {
-  return <h1>Header Component</h1>;
+  console.log(thumbnail);
+  return (
+    <div className={classes.container}>
+      <div className={classes["thumbnail-container"]}>
+        <img src={thumbnail.src} alt="thumbnail" />
+      </div>
+      <h1>My Portfolio</h1>
+      <button>Home</button>
+      <button>About</button>
+      <button>Projects</button>
+      <button>Contact</button>
+      <div>
+        <input type="text" placeholder="Search in site" />
+        <img src={icSearch.src} alt="ic-search" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
