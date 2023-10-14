5c3837b7f99775c518ce0e374a12576ab498bc3d
Mehdi <94266592+mehdi-repo@users.noreply.github.com>
2023-10-13 23:08:44 -0400

    complete css

--- pages/api/components/Client/ClientItem.jsx
+++ pages/api/components/Client/ClientItem.jsx
@@ -1,13 +1,16 @@
+import styles from "../../../../styles/Client/ClientItem.module.css";
+
 const ClientItem = ({ item }) => {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.client}>
         <img src={item.avatar} />
         <p>{item.title}</p>
-        <img src={item.stars} />
+        {/* <img className={styles.stars} src={item.stars} alt="review rate" /> */}
+        <p className={styles.stars}>⭐⭐⭐⭐⭐</p>
       </div>
       <p>{item.review}</p>
-      <p>{item.emoji}</p>
+      <p className={styles.emoji}>{item.emoji}</p>
     </div>
   );
 };
