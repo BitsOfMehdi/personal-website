87672f69198997f83678743f2ce8f9eeb7abd5dc
Mehdi <94266592+mehdi-repo@users.noreply.github.com>
2023-11-18 18:51:10 -0500

    initiate markups

--- pages/api/components/RecentPosts.jsx
+++ pages/api/components/RecentPosts.jsx
@@ -1,5 +1,36 @@
 const RecentPosts = () => {
-  return <h1>Recent Posts Component</h1>;
+  return (
+    <section className="recent-posts">
+      <div className="recent-posts__left-container">
+        <h2>Recent Posts</h2>
+        <p>Stay updated with my latest works and thoughts.</p>
+        <button>View All</button>
+      </div>
+      <div className="recent-posts__right-container">
+        <div className="recent-posts__right-container__item">
+          <img />
+          <p> Hello! How are you today?</p>
+          <div className="recent-posts__right-container__tag-container">
+            <span className="recent-posts__right-container__tag-container__tag">
+              <p className="recent-posts__right-container__tag-container__tag__text">
+                Design
+              </p>
+            </span>
+            <span className="recent-posts__right-container__tag-container__tag">
+              <p className="recent-posts__right-container__tag-container__tag__text">
+                Inspiration
+              </p>
+            </span>
+          </div>
+          <div className="recent-posts__right-container__item__avatar">
+            <p></p>
+            <img />
+          </div>
+          <div className="recent-posts__right-container__item__icon"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RecentPosts;
