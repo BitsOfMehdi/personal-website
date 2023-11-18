9f68e8ac5a9a923b4ab7c72393177eb7540390bc
Mehdi <94266592+mehdi-repo@users.noreply.github.com>
2023-11-18 18:23:08 -0500

    complete me-section

--- pages/api/components/Me.jsx
+++ pages/api/components/Me.jsx
@@ -2,17 +2,27 @@ const Me = () => {
   return (
     <section className="me">
       <div className="me__avatar"></div>
-      <div>
-        <h3>Vahid Alilou</h3>
-        <div>
-          <span>Designer</span>
-          <span>Developer</span>
+      <div className="me__middle-container">
+        <h3 className="me__middle-container__title">Vahid Alilou</h3>
+        <div className="me__middle-container__tag-container">
+          <span className="me__middle-container__tag-container__tag">
+            <p className="me__middle-container__tag-container__tag__text">
+              Designer
+            </p>
+          </span>
+          <span className="me__middle-container__tag-container__tag">
+            <p className="me__middle-container__tag-container__tag__text">
+              Developer
+            </p>
+          </span>
         </div>
-        <p>Passionate about creating intuitive and user-friendly interfaces.</p>
+        <p className="me__middle-container__paragraph">
+          Passionate about creating intuitive and user-friendly interfaces.
+        </p>
       </div>
-      <div>
-        <button></button>
-        <button></button>
+      <div className="me__button-container">
+        <button className="me__button-container__button--white">Contact</button>
+        <button className="me__button-container__button">Hire Me</button>
       </div>
     </section>
   );
