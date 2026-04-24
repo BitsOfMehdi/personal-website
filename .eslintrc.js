ba75f9f8e722141ccfa32b6c79a4c033048b8ba8
Mehdi Mousavi <smehdi.mousavikh@gmail.com>
2026-04-23 21:41:20 -0400

    Finalize TypeScript and ESLint configuration with modern settings and cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "next/typescript"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "no-unused-vars": "off",
+  },
+};
